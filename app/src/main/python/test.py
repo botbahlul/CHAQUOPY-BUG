81edbb92d0a39241041853f2a1f8f62cb2d36303
Bot Bahlul <88623122+botbahlul@users.noreply.github.com>
2023-03-06 23:38:50 +0800

    Add files via upload

--- app/src/main/python/test.py
+++ app/src/main/python/test.py
@@ -10,22 +10,27 @@ def show_progress_bar(activity, textview_debug):
     not_working_progressBar(100, 100, "Test : ", activity, textview_debug)
 
 
+#THIS FUNCTION GOT ERROR com.chaquo.python.PyException: NotImplementedError: com.chaquo.python.PyProxy._chaquopyGetDict is abstract and cannot be called
 def not_working_progressBar(count_value, total, prefix, activity, textview_debug):
     bar_length = 10
     filled_up_Length = int(round(bar_length*count_value/(total)))
     percentage = round(100.0 * count_value/(total),1)
     bar = '█' * filled_up_Length + '-' * (bar_length - filled_up_Length)
+
     class R(dynamic_proxy(Runnable)):
         def run(self):
             textview_debug.setText('%s|%10s|%3s%s\r' %(prefix, bar, int(percentage), '%'))
     activity.runOnUiThread(R())
 
 
+
+# THIS FUNCTION IS WORKING
 def working_progressBar(count_value, total, prefix, activity, textview_debug):
     bar_length = 10
     filled_up_Length = int(round(bar_length*count_value/(total)))
     percentage = round(100.0 * count_value/(total),1)
     bar = '█' * filled_up_Length + '-' * (bar_length - filled_up_Length)
+
     if (int(percentage) % 10 == 0):
         time.sleep(1)
         class R(dynamic_proxy(Runnable)):
